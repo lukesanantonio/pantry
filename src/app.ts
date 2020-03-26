eb4814251483aa2783d2119cea0c0619e9d46994
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2020-03-25 21:54:40 -0400

    Serve frontend statically from node

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,5 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('dist'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
