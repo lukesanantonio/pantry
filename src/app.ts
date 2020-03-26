aea1b97043d8ab9dad126c8c0274a45d7de41cd5
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2020-03-25 23:06:46 -0400

    Parcel to public/ so we don't serve the server

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,5 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static('dist'))
+app.use(express.static('dist/public'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
